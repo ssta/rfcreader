2d2a30ebebe8f00ad4f53e5c80483c1914f1cbd3
Stephen Stafford <clothcat@gmail.com>
2014-09-17 12:53:08 +0100

    now (shallowly) parses all the metadata

--- src/main/java/com/clothcat/rfcreader/IndexParser.java
+++ src/main/java/com/clothcat/rfcreader/IndexParser.java
@@ -42,10 +42,16 @@ public class IndexParser {
 
     private void parseChunk(String chunk) throws ParseException {
         Integer rfcnum;
-        String title;
+        String title = null;
         String authors = null;
         String date = null;
-        String status;
+        String status = null;
+        String format = null;
+        String obsoletes = null;
+        String obsoletedBy = null;
+        String updatedBy = null;
+        String updates = null;
+        String also = null;
 
         chunk = chunk.trim();
         // first 4 characters are the RFC number.
@@ -59,14 +65,50 @@ public class IndexParser {
             int pos1 = chunk.indexOf(". ");
             title = chunk.substring(0, pos1 + 1);
             chunk = chunk.substring(pos1 + 1);
-            Pattern datePattern = Pattern.compile("(January|February|March|April|May|June|July|August|September|October|November|December)\\s([0-9]{4}|[0-9] [0-9]{4})");
+            Pattern datePattern = Pattern.compile("(January|February|March|April|May|June|July|August|September|October|November|December)\\s([0-9]{4}|[0-9] [0-9]{4})\\.");
             Matcher m = datePattern.matcher(chunk);
             if (m.find()) {
                 int pos2 = m.start();
-                authors = chunk.substring(0, pos2);
-                date = chunk.substring(pos2, m.end());
+                authors = chunk.substring(0, pos2).trim();
+                int pos3 = m.end();
+                date = chunk.substring(pos2, pos3).trim();
+                chunk = chunk.substring(pos3).trim();
             }
-            System.out.println("" + rfcnum + "::" + title + "::" + authors + "::" + date + "\n" + chunk + "\n");
+            while (chunk.startsWith("(")) {
+                if (chunk.substring(1).toUpperCase().startsWith("FORMAT")) {
+                    int pos4 = chunk.indexOf(")");
+                    format = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                } else if (chunk.substring(1).toUpperCase().startsWith("STATUS")) {
+                    int pos4 = chunk.indexOf(")");
+                    status = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                } else if (chunk.substring(1).toUpperCase().startsWith("OBSOLETED BY")) {
+                    int pos4 = chunk.indexOf(")");
+                    obsoletedBy = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                } else if (chunk.substring(1).toUpperCase().startsWith("OBSOLETES")) {
+                    int pos4 = chunk.indexOf(")");
+                    obsoletes = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                } else if (chunk.substring(1).toUpperCase().startsWith("UPDATED BY")) {
+                    int pos4 = chunk.indexOf(")");
+                    updatedBy = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                } else if (chunk.substring(1).toUpperCase().startsWith("UPDATES")) {
+                    int pos4 = chunk.indexOf(")");
+                    updates = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                } else if (chunk.substring(1).toUpperCase().startsWith("ALSO")) {
+                    int pos4 = chunk.indexOf(")");
+                    also = chunk.substring(1, pos4);
+                    chunk = chunk.substring(pos4 + 1).trim();
+                }
+            }
+
+            System.out.println("" + rfcnum + "::" + title + "::" + authors
+                    + "::" + date + "::" + format + "::" + obsoletedBy + "::"
+                    + obsoletes + "::" + updatedBy + "::" + updates + "\n" + chunk + "\n");
         }
     }
 
